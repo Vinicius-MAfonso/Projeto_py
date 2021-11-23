2ff22080a75099f1c85525339eb9aeaf93c0becd
Vinicius Mariano <vinicius.mariano.afonso@gmail.com>
2021-11-23 16:00:35 -0300

    Vinicius Updates
    Updates no GUI

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 from tkinter import *
-import velocidade_de_digitacao
+import random
+import time
+
+
+def verificador():
+    if entry.get() == palavra_sorteada:
+        print("Acertou!")
+    else:
+        print("Errou")
+
+
+def countdown(num_of_secs):
+    contador = num_of_secs
+    while num_of_secs >= 0:
+        contador = contador - 1
+        time.sleep(1)
+        return str(contador)
+
+
+def sorteador():
+    palavras = ['manga', 'laranja', 'olá', 'banana', 'indore', 'hospital', 'python', 'java', 'ratlam',
+                'vaca', 'amarelo', 'preto', 'computador', 'azul']
+    random.shuffle(palavras)
+    return random.choice(palavras)
+
 
 window = Tk()
 window.title("Velocidade de digitação")
@@ -9,12 +33,28 @@ window.resizable(False, False)
 window.iconbitmap("images/icon.ico")
 window['bg'] = "#90e0ef"
 
-entry = Entry(window)
-entry.place(width=200,height = 25, x=155, y=150)
+lb_titulo = Label(window, text="Teste sua velocidade para digitar!", font="times 20", bg="#0077b6")
+lb_titulo.place(x=80, y=25)
 
-label = Label(text="Bem vindo ao jogo")
-btn_resetar_tempo = Button(window, text="Resetar o jogo", padx=50, pady=5)
-btn_resetar_tempo.pack(side=BOTTOM, padx=50, pady=50)
+lb_cont = Label(window, text=countdown(180), fg="white", bg="#0077b6")
+lb_cont.place(x=380, y=150)
+
+palavra_sorteada = sorteador()
+lb_palavra_sorteada = Label(window, text=palavra_sorteada, fg="white", font="times 18", bg="#0077b6")
+lb_palavra_sorteada.place(x=250, y=100)
+
+lb_score_title = Label(window, text="Pontuação", font="times 15", fg="black")
+lb_score_title.place(x=73, y=100)
+
+lb_score = Label(window, text="200", font="times 10", fg="black")
+lb_score.place(x=100, y=150)
+
+entry = Entry(window)
+entry.place(width=200, height=25, x=160, y=150)
+verificador()
+
+btn_resetar_tempo = Button(window, text="Resetar o jogo", padx=25, pady=5, bg="#caf0f8", command=countdown(180))
+btn_resetar_tempo.pack(side=BOTTOM, pady=50)
 
 window.mainloop()
 window.colorchooser.chooser()
