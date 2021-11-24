5c8ec62f2c25c5447e8f03747326a2ff925b6a5e
Vinicius Mariano <vinicius.mariano.afonso@gmail.com>
2021-11-24 10:02:11 -0300

    Vinicius updates
    GUI update, 'enter' como método para entrada de dados

--- main.py
+++ main.py
@@ -29,14 +29,15 @@ window['bg'] = "#023e8a"
 #         print(self.verpontos())
 
 
-def check_result():
+# botão enter
+def func(event):
     if entry.get() == words[word]:
         end = timer()
         print(end - start)
-        Pontuacao.ganhar()
+        # Pontuacao.ganhar()
     else:
         print("Wrong Input")
-        Pontuacao.perder()
+        # Pontuacao.perder()
 
 
 words = ['manga', 'laranja', 'olá', 'banana', 'indore', 'hospital', 'python', 'java', 'ratlam',
@@ -54,21 +55,29 @@ lb_titulo.place(x=65, y=25)
 
 entry = Entry(window)
 entry.place(width=200, height=25, x=160, y=147)
-
-lb_box = Label(window, bg="#0096c7", width=14, height=5)
-lb_box.place(x=15, y=95)
-
+# Pontuação
+lb_box_pontuacao = Label(window, bg="#0096c7", width=14, height=5)
+lb_box_pontuacao.place(x=15, y=95)
 lb_score_title = Label(window, text="Pontuação", font="times 15", fg="white", bg="#0096c7")
 lb_score_title.place(x=25, y=100)
-
 lb_score = Label(window, text="200", font="times 15", fg="yellow", bg="#0096c7")
 lb_score.place(x=49, y=145)
 
-btn_resetar_tempo = Button(window, text="Palavra feita!", font="arial 10", command=check_result, padx=5, pady=2,
-                           bg='#caf0f8')
-btn_resetar_tempo.place(x=370, y=146)
+# Cronometro
+lb_box_cronometro = Label(window, bg="#0096c7", width=14, height=5)
+lb_box_cronometro.place(x=384, y=95)
+lb_cronometro_title = Label(window, text="Tempo restante", font="times 12", fg="white", bg="#0096c7")
+lb_cronometro_title.place(x=385, y=100)
+lb_cronometro = Label(window, text="200", font="times 15", fg="yellow", bg="#0096c7")
+lb_cronometro.place(x=415, y=145)
 
-btn_resetar_tempo = Button(window, text="Resetar o jogo", font="arial 10", command=check_result, padx=15, pady=5,
+# Botão enter
+window.bind('<Return>', func)
+# Dica
+lb_dica = Label(window, text="Pressione 'enter' \npara confirmar a palavra!", font="times 10",bg = "#023e8a", fg="white")
+lb_dica.place(x=300, y=200)
+
+btn_resetar_tempo = Button(window, text="Resetar o jogo", font="arial 10", padx=15, pady=5,
                            bg='#caf0f8')
 btn_resetar_tempo.place(x=160, y=200)
 
