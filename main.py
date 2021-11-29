1f3faf3f207d965ca466ec85fe7873fe1e59c823
Vinicius Mariano <vinicius.mariano.afonso@gmail.com>
2021-11-29 19:32:04 -0300

    Vinicius Updates
    Janela resultados

--- main.py
+++ main.py
@@ -1,40 +1,57 @@
 from tkinter import *
 from timeit import default_timer as timer
 import random
+from threading import Timer
 
-window = Tk()
-window.title("Velocidade de digitação")
-window.geometry("500x300+200+200")
-window.resizable(False, False)
-window.iconbitmap("images/icon.ico")
-window['bg'] = "#023e8a"
-
-
-# class Pontuacao:
-#
-#     def __init__(self, pontos, ganhar, perder):
-#         self.pontos = 0
-#         self.perder = ganhar
-#         self.ganhar = perder
-#         self.verpontos = pontos
-#
-#     def perder(self):
-#         self.pontos -= 25
-#
-#     def ganhar(self):
-#         self.pontos += 50
-#
-#     def verpontos(self):
-#         self.verpontos()
-#         print(self.verpontos())
+root = Tk()
+root.title("Velocidade de digitação")
+root.geometry("500x300+200+200")
+root.resizable(False, False)
+root.iconbitmap("images/icon.ico")
+root['bg'] = "#023e8a"
 
 
-# botão enter
+def resultados(tempo):
+    niveis = ['Bom', 'Mediano', 'Ruim']
+    if tempo < 10:
+        resultado = niveis[0]
+    elif tempo >= 10 or tempo < 20:
+        resultado = niveis[1]
+    else:
+        resultado = niveis[2]
+    resultados = Tk()
+    resultados.title("Velocidade de digitação")
+    resultados.geometry("300x200+200+200")
+    resultados.resizable(False, False)
+    resultados.iconbitmap("images/icon.ico")
+    resultados['bg'] = "#023e8a"
+
+    lb_score_title = Label(resultados, text="Resultados", font="times 15", fg="white", bg="#0096c7")
+    lb_score_title.place(x=5, y=10)
+
+    lb_result = Label(resultados, text="Resultado geral:", font="times 12", fg="white", bg="#023e8a")
+    lb_result.place(x=5, y=50)
+
+    lb_result_result = Label(resultados, text=resultado, font="times 12", fg="white", bg="#023e8a")
+    lb_result_result.place(x=110, y=50)
+
+    lb_time = Label(resultados, text="Tempo levado:", font="times 12", fg="white", bg="#023e8a")
+    lb_time.place(x=5, y=80)
+
+    lb_time_result = Label(resultados, text=tempo, font="times 12", fg="white", bg="#023e8a")
+    lb_time_result.place(x=110, y=80)
+    btn_resetar_tempo = Button(resultados, text="Resetar o jogo", font="arial 10", padx=15, pady=5,
+                               bg='#caf0f8')
+    btn_resetar_tempo.place(x=80, y=150)
+
+    resultados.mainloop()
+
+
 def func(event):
     if entry.get() == words[word]:
         end = timer()
         print(end - start)
-        # Pontuacao.ganhar()
+        resultados(end-start)
     else:
         print("Wrong Input")
         # Pontuacao.perder()
@@ -47,38 +64,34 @@ word = random.randint(0, (len(words) - 1))
 
 start = timer()
 
-sorteado = Label(window, text=words[word], font="arial 20", bg="#90e0ef")
+sorteado = Label(root, text=words[word], font="arial 20", bg="#90e0ef")
 sorteado.place(x=160, y=85)
 
-lb_titulo = Label(window, text="Teste sua velocidade para digitar!", font="times 20", bg="#0077b6", fg="white")
+lb_titulo = Label(root, text="Teste sua velocidade para digitar!", font="times 20", bg="#0077b6", fg="white")
 lb_titulo.place(x=65, y=25)
 
-entry = Entry(window)
+entry = Entry(root)
 entry.place(width=200, height=25, x=160, y=147)
 # Pontuação
-lb_box_pontuacao = Label(window, bg="#0096c7", width=14, height=5)
+lb_box_pontuacao = Label(root, bg="#0096c7", width=14, height=5)
 lb_box_pontuacao.place(x=15, y=95)
-lb_score_title = Label(window, text="Pontuação", font="times 15", fg="white", bg="#0096c7")
+lb_score_title = Label(root, text="Pontuação", font="times 15", fg="white", bg="#0096c7")
 lb_score_title.place(x=25, y=100)
-lb_score = Label(window, text="200", font="times 15", fg="yellow", bg="#0096c7")
+lb_score = Label(root, text="200", font="times 15", fg="yellow", bg="#0096c7")
 lb_score.place(x=49, y=145)
 
 # Cronometro
-lb_box_cronometro = Label(window, bg="#0096c7", width=14, height=5)
+lb_box_cronometro = Label(root, bg="#0096c7", width=14, height=5)
 lb_box_cronometro.place(x=384, y=95)
-lb_cronometro_title = Label(window, text="Tempo restante", font="times 12", fg="white", bg="#0096c7")
+lb_cronometro_title = Label(root, text="Tempo restante", font="times 12", fg="white", bg="#0096c7")
 lb_cronometro_title.place(x=385, y=100)
-lb_cronometro = Label(window, text="200", font="times 15", fg="yellow", bg="#0096c7")
+lb_cronometro = Label(root, text="200", font="times 15", fg="yellow", bg="#0096c7")
 lb_cronometro.place(x=415, y=145)
 
 # Botão enter
-window.bind('<Return>', func)
+root.bind('<Return>', func)
 # Dica
-lb_dica = Label(window, text="Pressione 'enter' \npara confirmar a palavra!", font="times 10",bg = "#023e8a", fg="white")
-lb_dica.place(x=300, y=200)
+lb_dica = Label(root, text="Pressione 'enter' \npara confirmar a palavra!", font="times 10", bg="#023e8a", fg="white")
+lb_dica.place(x=190, y=200)
 
-btn_resetar_tempo = Button(window, text="Resetar o jogo", font="arial 10", padx=15, pady=5,
-                           bg='#caf0f8')
-btn_resetar_tempo.place(x=160, y=200)
-
-window.mainloop()
+root.mainloop()
