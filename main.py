435dcb1d708582747f58a7a0fdd1c217697714e5
Vinicius Mariano <vinicius.mariano.afonso@gmail.com>
2021-11-29 21:38:16 -0300

    Viniciu update

--- main.py
+++ main.py
@@ -1,14 +1,69 @@
 from tkinter import *
 from timeit import default_timer as timer
 import random
-from threading import Timer
+
+frases = ['A dúvida já é a resposta',
+          'Deixe pra trás o que não te leva pra frente',
+          'Se for pra desistir, desista de ser fraco',
+          'Aqui se faz, aqui se paga',
+          'Sem sacrifício, não há vitória',
+          'Você é o que escolhe ser',
+          'Gel de cabelo é muito gorduroso',
+          'O Feitiço das Gêmeas',
+          'Popularidade é desejada por todos',
+          'Eu sou alérgico a abelhas e amendoins',
+          'Meu presente preferido é chocolate',
+          'Apenas para sua informação',
+          'O meu navegador favorito',
+          'Esta é uma ideia muito boa',
+          ]
+
+
+def game():
+    start = timer()
+    def func(event):
+        if entry.get() == frases[word]:
+            end = timer()
+            resultados(end - start)
+        else:
+            print("Wrong Input")
+
+    root = Tk()
+    root.title("Velocidade de digitação")
+    root.geometry("500x300+200+200")
+    root.resizable(False, False)
+    root.iconbitmap("images/icon.ico")
+    root['bg'] = "#023e8a"
+
+    word = random.randint(0, (len(frases) - 1))
+
+    start = timer()
+
+    sorteado = Label(root, text=frases[word], font="arial 13", bg="white", justify=LEFT)
+    sorteado.place(width=300, height=25, x=95, y=80)
+
+    lb_titulo = Label(root, text="Comece a digitar!", font="times 20", bg="#0077b6", fg="white", )
+    lb_titulo.place(x=145, y=25)
+
+    entry = Entry(root, font="arial 15")
+    entry.place(width=280, height=25, x=100, y=200)
+    func(entry)
+    # Botão enter
+    root.bind('<Return>', func)
+    # Dica
+    lb_dica = Label(root, text="Pressione 'enter' \npara confirmar a palavra!", font="times 10", bg="#023e8a",
+                    fg="white")
+    lb_dica.place(x=170, y=250)
+
+    root.mainloop()
 
 
 def resultados(tempo):
+    start.quit()
     niveis = ['Bom', 'Mediano', 'Ruim']
-    if tempo < 10:
+    if tempo < 6:
         resultado = niveis[0]
-    elif tempo >= 10 or tempo < 20:
+    elif tempo >= 6 or tempo < 10:
         resultado = niveis[1]
     else:
         resultado = niveis[2]
@@ -34,70 +89,24 @@ def resultados(tempo):
     lb_time_result = Label(resultados, text=tempo, font="times 12", fg="white", bg="#023e8a")
     lb_time_result.place(x=110, y=80)
     btn_resetar_tempo = Button(resultados, text="Resetar o jogo", font="arial 10", padx=15, pady=5,
-                               bg='#caf0f8')
+                               bg='#caf0f8', command=game)
     btn_resetar_tempo.place(x=80, y=150)
 
     resultados.mainloop()
 
 
-def func(event):
-    if entry.get() == words[word]:
-        end = timer()
-        print(end - start)
-        resultados(end - start)
-    else:
-        print("Wrong Input")
-    # Pontuacao.perder()
+start = Tk()
+start.title("Velocidade de digitação")
+start.geometry("500x200+200+200")
+start.resizable(False, False)
+start.iconbitmap("images/icon.ico")
+start['bg'] = "#023e8a"
 
-
-def game(event):
-    root = Tk
-    root.mainloop()
-
-
-root = Tk()
-root.title("Velocidade de digitação")
-root.geometry("500x300+200+200")
-root.resizable(False, False)
-root.iconbitmap("images/icon.ico")
-root['bg'] = "#023e8a"
-
-words = ['Imagine uma nova história para \nsua vida e acredite nela',
-         'A alegria de fazer o bem é a única \nfelicidade verdadeira', 'A persistência é o caminho do êxito',
-         'Quem semeia a injustiça colherá \na desgraça', 'Deixa para trás o que não te leva \npara frente',
-         'A dúvida já é a resposta',
-         'Você viverá uma vida monótona e \nentediante se não correr riscos!',
-         'Eu não tenho sonhos, eu tenho \nobjetivos',
-         'Quanto mais alto, maior é a queda',
-         'A imaginação é mais \nimportante que o conhecimento']
-
-word = random.randint(0, (len(words) - 1))
-
-start = timer()
-
-sorteado = Label(root, text=words[word], font="arial 15", bg="#90e0ef", justify=CENTER)
-sorteado.place(x=100, y=80)
-
-lb_titulo = Label(root, text="Teste sua velocidade para digitar!", font="times 20", bg="#0077b6", fg="white", )
+lb_titulo = Label(start, text="Teste sua velocidade para digitar!", font="times 20", bg="#0077b6", fg="white", )
 lb_titulo.place(x=65, y=25)
 
-entry = Entry(root, font="arial 15")
-entry.place(width=280, height=25, x=100, y=200)
+btn_resetar_tempo = Button(start, text="Comece o jogo", font="arial 10", padx=15, pady=5,
+                           bg='#caf0f8', command=game)
+btn_resetar_tempo.place(x=180, y=90)
 
-# # Cronometro
-# lb_box_cronometro = Label(root, bg="#0096c7", width=14, height=5)
-# lb_box_cronometro.place(x=384, y=95)
-# lb_cronometro_title = Label(root, text="Tempo restante", font="times 12", fg="white", bg="#0096c7")
-# lb_cronometro_title.place(x=385, y=100)
-# lb_cronometro = Label(root, text="200", font="times 15", fg="yellow", bg="#0096c7")
-# lb_cronometro.place(x=415, y=145)
-
-# Botão enter
-
-root.bind('<Return>', func)
-# Dica
-lb_dica = Label(root, text="Pressione 'enter' \npara confirmar a palavra!", font="times 10", bg="#023e8a",
-                fg="white")
-lb_dica.place(x=170, y=250)
-
-root.mainloop()
+start.mainloop()
