cc3d7461ae850479906d4088d9f32298775ea761
Vinicius Mariano <vinicius.mariano.afonso@gmail.com>
2021-12-01 22:33:54 -0300

    asd
    asda

--- main.py
+++ main.py
@@ -8,11 +8,32 @@ def sair():
     start.destroy()
 
 
-def sair_game(event):
-    game.destroy()
-
-
 def game():
+    def resultados(tempo):
+
+        niveis = ['Bom', 'Mediano', 'Ruim']
+        if tempo < 6:
+            resultado = niveis[0]
+        elif tempo >= 6 or tempo < 10:
+            resultado = niveis[1]
+        else:
+            resultado = niveis[2]
+
+        lb_result = Label(root, text="Resultado geral:", font="times 12", fg="white", bg="#023e8a")
+        lb_result.place(x=120, y=120)
+
+        lb_result_result = Label(root, text=resultado, font="times 12", fg="white", bg="#023e8a")
+        lb_result_result.place(x=240, y=120)
+
+        lb_time = Label(root, text="Tempo levado:", font="times 12", fg="white", bg="#023e8a")
+        lb_time.place(x=120, y=150)
+
+        lb_time_result = Label(root, text=tempo, font="times 12", fg="white", bg="#023e8a")
+        lb_time_result.place(x=240, y=150)
+        btn_resetar_jogo = Button(root, text="Resetar o jogo", font="arial 10", padx=15, pady=5,
+                                  bg='#caf0f8', command=lambda: [game.destroy, game])
+        btn_resetar_jogo.place(x=350, y=250)
+
     timer = Timer()
 
     root = Tk()
@@ -36,7 +57,6 @@ def game():
         if entry.get() == frases[word]:
             end = Timer()
             resultados(end - timer)
-
         elif entry.get() != frases[word]:
             print("Palavra errada!")
 
@@ -48,52 +68,6 @@ def game():
                     fg="white")
     lb_dica.place(x=170, y=250)
     root.mainloop()
-
-
-# def erro():
-#     error = Tk()
-#     error.title("Velocidade de digitação")
-#     error.geometry("200x80+340+250")
-#     error.resizable(False, False)
-#     error.iconbitmap("images/icon.ico")
-#     lb_error_mensage = Button(error, text="Palavra errada", font="arial 15", bg="#0077b6", fg="white",
-#                               command=lambda: [error.destroy(),game(), game()])
-#     lb_error_mensage.place(x=30, y=25)
-#     error.mainloop()
-
-def resultados(tempo):
-    resultados = Tk()
-    niveis = ['Bom', 'Mediano', 'Ruim']
-    if tempo < 6:
-        resultado = niveis[0]
-    elif tempo >= 6 or tempo < 10:
-        resultado = niveis[1]
-    else:
-        resultado = niveis[2]
-    resultados.title("Velocidade de digitação")
-    resultados.geometry("300x200+200+200")
-    resultados.resizable(False, False)
-    resultados.iconbitmap("images/icon.ico")
-    resultados['bg'] = "#023e8a"
-
-    lb_score_title = Label(resultados, text="Resultados", font="times 15", fg="white", bg="#0096c7")
-    lb_score_title.place(x=5, y=10)
-
-    lb_result = Label(resultados, text="Resultado geral:", font="times 12", fg="white", bg="#023e8a")
-    lb_result.place(x=5, y=50)
-
-    lb_result_result = Label(resultados, text=resultado, font="times 12", fg="white", bg="#023e8a")
-    lb_result_result.place(x=110, y=50)
-
-    lb_time = Label(resultados, text="Tempo levado:", font="times 12", fg="white", bg="#023e8a")
-    lb_time.place(x=5, y=80)
-
-    lb_time_result = Label(resultados, text=tempo, font="times 12", fg="white", bg="#023e8a")
-    lb_time_result.place(x=110, y=80)
-    btn_resetar_jogo = Button(resultados, text="Resetar o jogo", font="arial 10", padx=15, pady=5,
-                              bg='#caf0f8', command=lambda: [sair_game()])
-    btn_resetar_jogo.place(x=80, y=150)
-    resultados.mainloop()
 
 
 start = Tk()
